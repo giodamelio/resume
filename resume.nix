1e93c8103b84e32066b35868736f0496bb78fe4a
Giovanni d'Amelio <gio@damelio.net>
2024-11-01 23:40:40 -0700

    Init JSON resume flake and convert my resume to nix

---
+++ resume.nix
@@ -0,0 +1,178 @@
+{
+  basics = {
+    name = "Giovanni d'Amelio";
+    label = "Software Developer";
+    email = "gio@damelio.net";
+    phone = "1.206.372.3323";
+    website = "https://giodamelio.com";
+    summary = "I am a jack of all trades developer who enjoys building reliable, scalable services as much as fine-tuning deployment pipelines. I've gained a broad range of skills working on small, dynamic teams and thrive when I get to wear multiple hats. I'm passionate about open-source software, which I've relied on almost exclusively for years, and I'm always excited to pick up new languages or frameworks—whether it's for work or just for fun.";
+    location = {
+      postalCode = "98121";
+      city = "Seattle";
+      countryCode = "US";
+      region = "WA";
+    };
+    profiles = [
+      {
+        network = "LinkedIn";
+        username = "giodamelio";
+        url = "https://www.linkedin.com/in/giodamelio";
+      }
+      {
+        network = "Github";
+        username = "giodamelio";
+        url = "https://github.com/giodamelio";
+      }
+    ];
+  };
+  work = [
+    {
+      name = "BackNine Insurance and Financial Services";
+      position = "Senior Software Developer";
+      startDate = "2024-04";
+      endDate = "2024-11";
+      summary = "Led major projects in infrastructure and process automation, modernizing systems to boost both security and productivity.";
+      highlights = [
+        "Migrated infrastructure to AWS using ECS, Terraform, Docker, and GitHub Actions, streamlining deployments and enhancing scalability."
+        "Revamped the email system to support flexible routing, reducing workload for case agents and speeding up response times."
+        "Automated key security tasks, ensuring compliance with minimal manual work."
+        "Improved developer experience by optimizing workflows and tools, enabling faster and more consistent code delivery."
+      ];
+    }
+    {
+      name = "Square";
+      position = "Senior Software Developer (L5)";
+      startDate = "2021-09";
+      endDate = "2023-01";
+      summary = "Worked on the signup web app to ensure smooth multi-country compliance and improve data accuracy through robust state management.";
+      highlights = [
+        "Built solutions to support business expansion into new markets."
+        "Developed tools for rapid rollout of marketing referral programs."
+        "Led a project to enable automated, large-scale targeted marketing."
+        "Built a webhook allowing non-technical staff to create referral campaigns independently."
+        "Enhanced security measures in internal tools to safeguard promotions."
+        "Modified multiple services to support new business verticals in regulated markets."
+      ];
+    }
+    {
+      name = "Avalara";
+      position = "Software Developer (SDE2)";
+      startDate = "2017-09";
+      endDate = "2021-08";
+      summary = "Collaborated on Node.js and TypeScript microservices deployed on AWS Lambda, enhancing interoperability across products.";
+      highlights = [
+        "Led a CI/CD overhaul during the GitLab migration, improving usability and speed."
+        "Served as Security Champion, driving secure design and implementation across the team."
+        "Worked closely with SECENG and Security Champions to implement security best practices."
+      ];
+    }
+    {
+      name = "Code Fellows School";
+      position = "Lead Teaching Assistant";
+      startDate = "2016-11";
+      endDate = "2017-08";
+      summary = "Supported students in mastering advanced JavaScript concepts, debugging, and refining their skills through detailed feedback.";
+      highlights = [
+        "Taught foundational Node.js, RESTful APIs, and single-page app development with Angular.js."
+        "Provided hands-on debugging help during labs and final projects, enhancing student comprehension."
+      ];
+    }
+  ];
+  education = [
+    {
+      institution = "Code Fellows";
+      area = "Advanced Full-Stack JavaScript Development";
+      studyType = "Certificate";
+      startDate = "2016-07";
+      endDate = "2016-09";
+    }
+  ];
+  skills = [
+    {
+      name = "Programming Languages";
+      keywords = [
+        "TypeScript"
+        "JavaScript"
+        "Golang"
+        "Ruby"
+        "Python"
+        "Elixir"
+        "Rust"
+        "Clojure"
+      ];
+    }
+    {
+      name = "Backend Frameworks";
+      keywords = [
+        "Express"
+        "Rails"
+        "Django"
+        "GRPC"
+      ];
+    }
+    {
+      name = "Frontend Frameworks";
+      keywords = [
+        "React"
+        "Ember"
+        "Vue"
+        "HTMx"
+        "Redux"
+      ];
+    }
+    {
+      name = "Databases";
+      keywords = [
+        "MySQL"
+        "AWS Aurora"
+        "PostgreSQL"
+        "Redis"
+        "SQLite"
+        "SurrealDB"
+      ];
+    }
+    {
+      name = "Testing";
+      keywords = [
+        "TDD"
+        "Jest"
+        "FactoryBot"
+        "Various mocking libraries"
+        "RSpec"
+      ];
+    }
+    {
+      name = "Tooling";
+      keywords = [
+        "Git"
+        "Vim"
+        "Shell scripting"
+        "Linux"
+      ];
+    }
+    {
+      name = "Deployment";
+      keywords = [
+        "AWS"
+        "Terraform"
+        "Docker"
+        "Podman"
+        "Packer"
+        "Linux"
+      ];
+    }
+    {
+      name = "AWS Services";
+      keywords = [
+        "Lambda"
+        "ECS"
+        "EC2"
+        "API Gateway"
+        "SQS"
+        "Route53"
+        "S3"
+        "IAM"
+      ];
+    }
+  ];
+}

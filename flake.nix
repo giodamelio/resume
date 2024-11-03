e69b77662224d21e61e5f3bd758a800285aef8b7
Giovanni d'Amelio <gio@damelio.net>
2024-11-02 22:36:36 -0700

    Add new flake-parts based flake.nix from template

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Render cool resumes from a data file";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        packages.default = pkgs.hello;
+      };
+      flake = {};
+    };
+}
